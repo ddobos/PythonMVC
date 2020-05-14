31e1f4c3bb85a96a3e6e77f5441ce7fa232d890b
Denis Dobos <ddobos40@gmail.com>
2020-05-14 17:05:45 +0300

    add models Post

--- apps/models.py
+++ apps/models.py
@@ -1,3 +1,14 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    create_at = models.DataTimeFiled(auto_now_add=True)
+    update_at = models.DataTimeFiled(auto_now=True)
+
+    def __str__(self):
+        return f'{self.title} {self.author.first_name}'
+
